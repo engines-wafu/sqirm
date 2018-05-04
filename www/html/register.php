05c0640c6e6f1c28d370898b148bb6602791748e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 02:50:21 -0700

    Modified register.php

--- www/html/register.php
+++ www/html/register.php
@@ -67,14 +67,10 @@ if (isset($_POST['reg_user'])) {
 			<div style = "margin:30px">
 
         <form method="post" action="register.php">
-  	      <label>Username</label>
-  	      <input type="text" name="username" value="<?php echo $username; ?>">
-  	      <label>Email</label>
-  	      <input type="email" name="email" value="<?php echo $email; ?>">
-  	      <label>Password</label>
-  	      <input type="password" name="password_1">
-  	      <label>Confirm password</label>
-  	      <input type="password" name="password_2">
+  	      <label>Username</label><input type="text" name="username" value="<?php echo $username; ?>" class = "box"/><br /><br />
+  	      <label>Email</label><input type="email" name="email" value="<?php echo $email; ?>" class = "box"/><br /><br />
+  	      <label>Password</label><input type="password" name="password_1" class = "box"/><br /><br />
+  	      <label>Confirm password</label><input type="password" name="password_2" class = "box"/><br /><br />
   	      <button type="submit" class="btn" name="reg_user">Register</button>
   			</form>
 
