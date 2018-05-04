fccbea0fd136b05c515f7cc07a2f296f2b558351
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 16:59:01 -0700

    Still trying to get the SQL data into chart.js

--- www/html/line-db-php.js
+++ www/html/line-db-php.js
@@ -1,18 +1,38 @@
 $(document).ready(function() {
 
-		/**
-		 * call the data.php file to fetch the result from db table.
-		 */
-		$.ajax({
-				url : "http://ec2-13-56-14-28.us-west-1.compute.amazonaws.com/api/data.php",
-				type : "GET",
-				success : function(data){
-						console.log(data);
-				},
-				error : function(data) {
-						console.log(data);
-				}
-		});
+  /**
+   * call the data.php file to fetch the result from db table.
+   */
+  $.ajax({
+    url : "http://ec2-13-56-14-28.us-west-1.compute.amazonaws.com/api/data.php",
+    type : "GET",
+    success : function(data){
+      console.log(data);
+
+      var kpivals = {
+        kpiVal1 : [],
+        kpiVal2 : [],
+        kpiVal3 : []
+      };
+
+      var len = data.length;
+
+      for (var i = 0; i < len; i++) {
+        if (data[i].kpiDesc == "Self Audits") {
+          kpiVal.SAs.push(data[i].kpiVal);
+        }
+        else if (data[i].kpiDesc == "Open Actions") {
+          kpiVal.OAs.push(data[i].kpiVal);
+        }
+      }
+
+      console.log(kpiVals);
+
+    },
+    error : function(data) {
+      console.log(data);
+    }
+  });
 
 });
 
