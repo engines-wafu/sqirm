05fe9f9e3efaa0180486d727e6d5e643f2ce66ef
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 08:11:48 -0700

    Trying to fix login and registration.php

--- www/html/login.php
+++ www/html/login.php
@@ -12,15 +12,10 @@
 <body>
 <div class="content"><h1>Login Page</h1></div>
 <form method="post" action="login.php">
-<div class="container">
 <label>Username</label> <input type="text" name="username"> <br />
 <label>Password</label> <input type="password" name="password"> <br />
 <button type="submit" class="btn" name="login_user">Login</button> <br />
-</div>
-<p>
-<a href="register.php">Sign up</a>
-</p>
+<p> <a href="register.php">Sign up</a> </p>
 </form>
-	</div>
 </body>
 </html>
