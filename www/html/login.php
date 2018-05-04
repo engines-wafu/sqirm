737c512e85fabbf801f9ef51b410644d31777b8d
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 03:37:14 -0700

    Modified login.php and created server.php

--- www/html/login.php
+++ www/html/login.php
@@ -1,35 +1,12 @@
 <?php
   include "../inc/dbinfo.inc"; 
   session_start();
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-   // username and password sent from form
-
-   $username = mysqli_real_escape_string($db,$_POST['username']);
-   $password = mysqli_real_escape_string($db,$_POST['password']);
-
-   $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-   $result = mysqli_query($connection, $sql);
-   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-   $active = $row['active'];
-
-   $count = mysqli_num_rows($result);
-
-   // If result matched $myusername and $mypassword, table row must be 1 row
-
-   if($count == 1) {
-      session_register("myusername");
-      $_SESSION['login_user'] = $myusername;
-
-      header("location: welcome.php");
-   }else {
-      $error = "Your Login Name or Password is invalid";
-   }
-}
 ?>
 <html>
   <head>
     <style>
       <?php include "class.css"; ?>
+      <?php include "server.php"; ?>
     </style>
   </head>
 <body>
