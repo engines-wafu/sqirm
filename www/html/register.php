9df429d7de3c3f675623ca7f57a77bc2f3cf0b65
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 02:40:19 -0700

    Modified register.php

--- www/html/register.php
+++ www/html/register.php
@@ -8,6 +8,59 @@
 <body>
 <h1>Registration Page</h1>
 
+<?php
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($connection, $_POST['username']);
+  $email = mysqli_real_escape_string($connection, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($connection, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($connection, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...  by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($connection, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
+  	mysqli_query($connection, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+?>
+
   <form method="post" action="register.php">
   	<div class="input-group">
   	  <label>Username</label>
