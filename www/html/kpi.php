4fbd2bd728b10280aa2eab997e3c0508b4b58a1e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 12:44:49 -0700

    Testing chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -27,11 +27,13 @@
   <article style="border: none;"> </article>
   <article>
     <div>
-      <?php
-        while ($row = $mysqli_fetch_array($result)) {
-          echo $row['kpiDesc'];
-        }
-      ?>
+      <p>
+        <?php
+          while ($row = mysqli_fetch_array($result)) {
+            echo $row['kpiDesc'];
+          }
+        ?>
+      </p>
       <canvas id="myChart" width="150" height="150"></canvas>
       <script src=Chart.js></script>
       <script>
