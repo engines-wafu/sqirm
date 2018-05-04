02960b42d2b4715b67f98b3134d7bd93b27f58d9
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 08:10:17 -0700

    Trying to fix login and registration.php

--- www/html/login.php
+++ www/html/login.php
@@ -10,27 +10,17 @@
     </style>
   </head>
 <body>
-<h1>Login Page</h1>
-
-	<div align = "center">
-    <div style = "width:300px; border: solid 3px black; " align = "left">
-      <div style = "background-color:black; color:white; padding:10px;"><h2>Login</h2></div>
-			<div style = "margin:50px">
-
-        <form method="post" action="login.php">
-					<article class="container">
-				    <label>Username</label> <input type="text" name="username"> <br />
-         	  <label>Password</label> <input type="password" name="password"> <br />
-         	  <button type="submit" class="btn" name="login_user">Login</button> <br />
-				  </article>
-         	<p>
-         		Not yet a member? <a href="register.php">Sign up</a>
-         	</p>
-       	</form>
-
-			  <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php include('errors.php'); ?></div>
-			</div>
-	  </div>
+<div class="content"><h1>Login Page</h1></div>
+<form method="post" action="login.php">
+<div class="container">
+<label>Username</label> <input type="text" name="username"> <br />
+<label>Password</label> <input type="password" name="password"> <br />
+<button type="submit" class="btn" name="login_user">Login</button> <br />
+</div>
+<p>
+<a href="register.php">Sign up</a>
+</p>
+</form>
 	</div>
 </body>
 </html>
