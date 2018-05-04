2a1d59fad9082ceaa3dfbd3194c23bb1fc874c5f
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 04:00:04 -0700

    Modified session.php and added session check to RiskView.php

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -22,8 +22,8 @@
     
         <!-- logged in user information -->
         <?php  if (isset($_SESSION['username'])) : ?>
-        	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-        	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+        	<p>Welcome <?php echo $_SESSION['username']; ?></p>
+        	<p> <a href="welcome.php?logout='1'" style="color: red;">logout</a> </p>
         <?php endif ?>
       </div>
 
