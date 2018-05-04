5e6d02b1fa871661335d4e38ef4dff61961f4781
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 03:52:58 -0700

    Modified welcome.php

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -1,6 +1,17 @@
 <?php
-   include('session.php');
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
 ?>
+
 <html>
   <head>
     <style>
@@ -9,7 +20,26 @@
   </head>
   <body>
    	<h1>Welcome Page</h1>
-   	<h1>Welcome <?php echo $login_session; ?></h1> 
+      <div class="content">
+      	<!-- notification message -->
+      	<?php if (isset($_SESSION['success'])) : ?>
+          <div class="error success" >
+          	<h3>
+              <?php 
+              	echo $_SESSION['success']; 
+              	unset($_SESSION['success']);
+              ?>
+          	</h3>
+          </div>
+      	<?php endif ?>
+    
+        <!-- logged in user information -->
+        <?php  if (isset($_SESSION['username'])) : ?>
+        	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+        	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+        <?php endif ?>
+      </div>
+
    	<h2><a href = "logout.php">Sign Out</a></h2>
   </body>
 </html>
