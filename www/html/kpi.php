ce93c42e93c9cef23e592d092d1324c895d6990e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 14:31:32 -0700

    Still trying to get the SQL data into chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -15,48 +15,6 @@
       <canvas id="myChart" width="150" height="150"></canvas>
       <script src=Chart.js></script>
       <?php include "../js/doughnut.js"; ?>
-	  <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-          type: 'doughnut',
-          data: {
-            labels: ["Green", "Yellow", "Red"],
-            datasets: [{
-              data: [12, 19, 3],
-              backgroundColor: [
-                'green',
-                'yellow',
-                'red',
-              ],
-              borderColor: [
-                'black',
-                'black',
-                'black',
-              ],
-              borderWidth: 3
-            }]
-          },
-          options: {
-            circumference: Math.PI,
-            rotation: 1.0 * Math.PI,
-            percentageInnerCutout: 10,
-            responsive: false,
-            legend: {
-              display: false
-            },
-            title: {
-              display: true,
-              fontFamily: 'Arial',
-              fontColor: 'black',
-              lineHeight: 1,
-              text: 'Self Audits'
-            },
-            layout:{
-              padding:40
-            },
-          }
-      });
-      </script>
     </div>
   </article>
   <article style="border: none;"> </article>
--- www/js/doughnut.js
+++ www/js/doughnut.js
@@ -1,1 +1,42 @@
-echo 'cock';
+	  <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: ["Green", "Yellow", "Red"],
+            datasets: [{
+              data: [12, 19, 3],
+              backgroundColor: [
+                'green',
+                'yellow',
+                'red',
+              ],
+              borderColor: [
+                'black',
+                'black',
+                'black',
+              ],
+              borderWidth: 3
+            }]
+          },
+          options: {
+            circumference: Math.PI,
+            rotation: 1.0 * Math.PI,
+            percentageInnerCutout: 10,
+            responsive: false,
+            legend: {
+              display: false
+            },
+            title: {
+              display: true,
+              fontFamily: 'Arial',
+              fontColor: 'black',
+              lineHeight: 1,
+              text: 'Self Audits'
+            },
+            layout:{
+              padding:40
+            },
+          }
+      });
+      </script>
