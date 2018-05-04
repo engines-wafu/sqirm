4a4b49de79ff6853e67b2ae7714e20036d16422d
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 02:12:05 -0700

    Modified welcome.php

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -3,9 +3,12 @@
 ?>
 <html>
   <head>
-   	<h1>Welcome Page</h1>
+    <style>
+      <?php include "class.css"; ?>
+    </style>
   </head>
   <body>
+   	<h1>Welcome Page</h1>
    	<h1>Welcome <?php echo $login_session; ?></h1> 
    	<h2><a href = "logout.php">Sign Out</a></h2>
   </body>
