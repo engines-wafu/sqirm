b5727b44d9fa21c5496a40b9a9815ee40f85e7ca
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 09:54:13 -0700

    Testing chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -16,7 +16,6 @@
       <script src=Chart.js></script>
       <script>
       Chart.defaults.doughnut.cutoutPercentage = '80';
-      Chart.defaults.doughnut.rotation = '2.3562';
       Chart.defaults.doughnut.rotation = '4.7124';
       var ctx = document.getElementById("myChart");
       var myChart = new Chart(ctx, {
