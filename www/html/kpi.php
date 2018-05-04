2304fb5079088681ac5257ec09ee618723917a55
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 09:36:29 -0700

    Testing chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -15,6 +15,7 @@
       <canvas id="myChart" width="150px" height="150px"></canvas>
       <script src=Chart.js></script>
       <script>
+      Chart.defaults.doughnut.cutoutPercentage = '80';
       var ctx = document.getElementById("myChart");
       var myChart = new Chart(ctx, {
           type: 'doughnut',
@@ -38,7 +39,6 @@
           },
           options: {
             scales: { },
-            cutoutPercentage: {80},
             legend: { 
                 display: false,
             }
