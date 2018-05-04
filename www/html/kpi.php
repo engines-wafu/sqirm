d90e4ffb06138998140b293a9dd02f4b2985210d
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 14:14:26 -0700

    Still trying to get the SQL data into chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -14,48 +14,7 @@
     <div>
       <canvas id="myChart" width="150" height="150"></canvas>
       <script src=Chart.js></script>
-      <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-          type: 'doughnut',
-          data: {
-            labels: ["Green", "Yellow", "Red"],
-            datasets: [{
-              data: [12, 19, 3],
-              backgroundColor: [
-                'green',
-                'yellow',
-                'red',
-              ],
-              borderColor: [
-                'black',
-                'black',
-                'black',
-              ],
-              borderWidth: 3
-            }]
-          },
-          options: {
-            circumference: Math.PI,
-            rotation: 1.0 * Math.PI,
-            percentageInnerCutout: 10,
-            responsive: false,
-            legend: {
-              display: false
-            },
-            title: {
-              display: true,
-              fontFamily: 'Arial',
-              fontColor: 'black',
-              lineHeight: 1,
-              text: 'Self Audits'
-            },
-            layout:{
-              padding:40
-            },
-          }
-      });
-      </script>
+      <script src=../js/doughnut.js></script>
     </div>
   </article>
   <article style="border: none;"> </article>
