7dc57333df808f6b0282f65ce5a3d0904ee2afa3
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 14:19:35 -0700

    Still trying to get the SQL data into chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -14,7 +14,7 @@
     <div>
       <canvas id="myChart" width="150" height="150"></canvas>
       <script src=Chart.js></script>
-      <script src=doughnut.js></script>
+      <script src=../js/doughnut.js></script>
 	  <script>
       var ctx = document.getElementById("myChart");
       var myChart = new Chart(ctx, {
