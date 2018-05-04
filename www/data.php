942eaecb07ce67bbbbf816f8b3a6b787f990fe89
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 13:12:50 -0700

    Attempting to get SQL data into chart.js

---
+++ www/data.php
@@ -0,0 +1,34 @@
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+include "../inc/dbinfo.inc";
+
+//get connection
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if(!$mysqli){
+	die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT * FROM kpis");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
