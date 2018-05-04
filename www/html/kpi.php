cb16a847143fbdaf79ebacae8f66b12bff760b3e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 08:58:34 -0700

    Testing chart.js

---
+++ www/html/kpi.php
@@ -0,0 +1,19 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<html>
+  <head>
+    <style>
+      <?php include "class.css"; ?>
+      <?php include "server.php"; ?>
+    </style>
+  </head>
+<body>
+<div class="content"><h1>KPI Test Page</h1></div>
+<section>
+  <article style="border: none;"> </article>
+  <article>
+    <canvas id="myChart" width="150" height="150"></canvas>
+  </article>
+  <article style="border: none;"> </article>
+</section>
+</body>
+</html>
