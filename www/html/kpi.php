69ab7922f918e41ddc0cca933d6e22ed905404bb
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-04 11:39:23 -0700

    Testing chart.js

--- www/html/kpi.php
+++ www/html/kpi.php
@@ -29,15 +29,18 @@
                 'green',
               ],
               borderColor: [
-                'red',
-                'yellow',
-                'green',
+                'black',
+                'black',
+                'black',
               ],
-              borderWidth: 1
+              borderWidth: 3
             }]
           },
           options: {
-            scales: { },
+            title: {
+              display: true,
+              text: 'Status of Self Audits'
+            },
             legend: { 
                 display: false,
             }
