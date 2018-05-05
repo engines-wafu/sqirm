ed081826accd38ce7320858456f7e082ba398969
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 07:13:04 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -5,68 +5,68 @@ $(document).ready(function(){
     success: function(data) {
       console.log(data);
 
-      var kpiDesc = [];
-      var kpiVal1 = [];
-      var kpiVal2 = [];
-      var kpiVal3 = [];
-      var kpiArr = [];
-      var kpiData = [];
+      //var kpiDesc = [];
+      //var kpiVal1 = [];
+      //var kpiVal2 = [];
+      //var kpiVal3 = [];
+      //var kpiArr = [];
+      //var kpiData = [];
 
-      for(var i in data) {
-        kpiDesc.push(data[i].kpiDesc);
-        do {
-          kpiData.push(data[i].kpiVal1);
-          kpiData.push(data[i].kpiVal2);
-          kpiData.push(data[i].kpiVal3);
-        }
-        while (kpiDesc = i);
-      }
+      //for(var i in data) {
+      //  kpiDesc.push(data[i].kpiDesc);
+      //  do {
+      //    kpiData.push(data[i].kpiVal1);
+      //    kpiData.push(data[i].kpiVal2);
+      //    kpiData.push(data[i].kpiVal3);
+      //  }
+      //  while (kpiDesc = i);
+      //}
 
-      console.log(kpiDesc);
-      console.log(kpiData);
+      //console.log(kpiDesc);
+      //console.log(kpiData);
 
-      var chartdata = {
-        labels: ["In Limits", "Near Limits", "Out of Limits"],
-        datasets : [
-          {
-            label: 'KPI Score',
-            backgroundColor: [
-              'green',
-              'yellow',
-              'red'
-            ],
-            borderColor: 'black',
-            borderWidth: 3,
-            data: kpiArr
-          }
-        ]
-      };
+      //var chartdata = {
+      //  labels: ["In Limits", "Near Limits", "Out of Limits"],
+      //  datasets : [
+      //    {
+      //      label: 'KPI Score',
+      //      backgroundColor: [
+      //        'green',
+      //        'yellow',
+      //        'red'
+      //      ],
+      //      borderColor: 'black',
+      //      borderWidth: 3,
+      //      data: kpiArr
+      //    }
+      //  ]
+      //};
 
-      var ctx = $("#kpicanvas") + kpiID[i];
+      //var ctx = $("#kpicanvas") + kpiID[i];
 
-      var Graph = new Chart(ctx, {
-        type: 'doughnut',
-        data: chartdata,
-        options: {
-          circumference: Math.PI,
-          rotation: 1.0 * Math.PI,
-          percentageInnerCutout: 10,
-          responsive: false,
-          legend: {
-            display: false
-          },
-          title: {
-            display: true,
-            fontFamily: 'Arial',
-            fontColor: 'black',
-            lineHeight: 1,
-            text: kpiDesc
-          },
-          layout:{
-            padding:40
-          },
-        }
-      });
+      //var Graph = new Chart(ctx, {
+      //  type: 'doughnut',
+      //  data: chartdata,
+      //  options: {
+      //    circumference: Math.PI,
+      //    rotation: 1.0 * Math.PI,
+      //    percentageInnerCutout: 10,
+      //    responsive: false,
+      //    legend: {
+      //      display: false
+      //    },
+      //    title: {
+      //      display: true,
+      //      fontFamily: 'Arial',
+      //      fontColor: 'black',
+      //      lineHeight: 1,
+      //      text: kpiDesc
+      //    },
+      //    layout:{
+      //      padding:40
+      //    },
+      //  }
+      //});
     },
     error: function(data) {
       console.log(data);
