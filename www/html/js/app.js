3ef1b3a3c6afd9db7bb1471a642f6fac4cbb9937
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 08:17:41 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
       var kpiCanvas = [];
       
       for(var i in data){
-        ctx = ctemp + kpiID[i];
+        ctx = ctemp + kpiIDs[i];
         kpiCanvas.push(ctx);
       }
       
