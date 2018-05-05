491828f75872974c6d287e9ea8068be0123c1a27
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 10:24:06 -0700

    Working on multiple kpis

--- www/html/graph.php
+++ www/html/graph.php
@@ -25,9 +25,7 @@ $database = mysqli_select_db($connection, DB_DATABASE);
       $query = "SELECT kpiID FROM kpis";
       $result = mysqli_query($connection, $query); 
       while ($row = mysqli_fetch_array($result)) {
-        echo '<div id="chart-container">';
-        echo '   <canvas id="#kpicanvas' . $row['kpiID'] . '"></canvas>';
-        echo '</div>';
+        echo '<canvas id="#kpicanvas' . $row['kpiID'] . '"></canvas>';
       }
     ?>
 
