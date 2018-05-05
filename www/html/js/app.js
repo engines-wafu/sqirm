2275e8f97a908f2bb6090f454ed8dbde14271b1e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 09:08:06 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -57,72 +57,48 @@ $(document).ready(function(){
         kpidatasetarray[i].push(kpiVal1[i], kpiVal2[i], kpiVal3[i]);
         console.log(kpidatasetarray[i]);
       }
-
+      var chartdata = {
+        labels: ["In Limits", "Near Limits", "Out of Limits"],
+        datasets : [
+          {
+            label: 'KPI Score',
+            backgroundColor: [
+              'green',
+              'yellow',
+              'red'
+            ],
+            borderColor: 'black',
+            borderWidth: 3,
+            data: kpidatasetarray[0]
+          }
+        ]
+      };
+      var Graph = new Chart(kpiCanvas[0], {
+        type: 'doughnut',
+        data: chartdata,
+        options: {
+          circumference: Math.PI,
+          rotation: 1.0 * Math.PI,
+          percentageInnerCutout: 10,
+          responsive: false,
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            fontFamily: 'Arial',
+            fontColor: 'black',
+            lineHeight: 1,
+            text: kpiDesc
+          },
+          layout:{
+            padding:40
+          },
+        }
+      });
     },
     error: function(data) {
     }
   });
 });
 
-      //var kpiDesc = [];
-      //var kpiVal1 = [];
-      //var kpiVal2 = [];
-      //var kpiVal3 = [];
-      //var kpiArr = [];
-      //var kpiData = [];
-
-      //for(var i in data) {
-      //  kpiDesc.push(data[i].kpiDesc);
-      //  do {
-      //    kpiData.push(data[i].kpiVal1);
-      //    kpiData.push(data[i].kpiVal2);
-      //    kpiData.push(data[i].kpiVal3);
-      //  }
-      //  while (kpiDesc = i);
-      //}
-
-      //console.log(kpiDesc);
-      //console.log(kpiData);
-
-      //var chartdata = {
-      //  labels: ["In Limits", "Near Limits", "Out of Limits"],
-      //  datasets : [
-      //    {
-      //      label: 'KPI Score',
-      //      backgroundColor: [
-      //        'green',
-      //        'yellow',
-      //        'red'
-      //      ],
-      //      borderColor: 'black',
-      //      borderWidth: 3,
-      //      data: kpiArr
-      //    }
-      //  ]
-      //};
-
-      //var ctx = $("#kpicanvas") + kpiID[i];
-
-      //var Graph = new Chart(ctx, {
-      //  type: 'doughnut',
-      //  data: chartdata,
-      //  options: {
-      //    circumference: Math.PI,
-      //    rotation: 1.0 * Math.PI,
-      //    percentageInnerCutout: 10,
-      //    responsive: false,
-      //    legend: {
-      //      display: false
-      //    },
-      //    title: {
-      //      display: true,
-      //      fontFamily: 'Arial',
-      //      fontColor: 'black',
-      //      lineHeight: 1,
-      //      text: kpiDesc
-      //    },
-      //    layout:{
-      //      padding:40
-      //    },
-      //  }
-      //});
