49e862057f2607ef3c40237d25ce998551aae05b
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 07:23:38 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
     url: "http://ec2-13-56-14-28.us-west-1.compute.amazonaws.com/api/data.php",
     method: "GET",
     success: function(data) {
-      console.log(data);
     },
     error: function(data) {
-      console.log(data);
     }
   });
 });
