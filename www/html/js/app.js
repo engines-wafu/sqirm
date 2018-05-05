4f44f15d7ca6cf5c253c3595e1d725da6ed184cc
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-05 06:37:57 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -4,7 +4,7 @@ console.log(kpiID);
 for(var i in kpiID) {
   $(document).ready(function(){
   $.ajax({
-    url: "http://ec2-13-56-14-28.us-west-1.compute.amazonaws.com/api/data.php?kpiID=" + kpiID[0] ,
+    url: "http://ec2-13-56-14-28.us-west-1.compute.amazonaws.com/api/data.php?kpiID=" + kpiID[i] ,
     method: "GET",
     success: function(data) {
       console.log(data);
