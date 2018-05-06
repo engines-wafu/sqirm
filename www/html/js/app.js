7f93f89c6a75821bbedc9997270388c94435adb5
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 08:18:48 -0700

    Working on multiple kpis

--- www/html/js/app.js
+++ www/html/js/app.js
@@ -79,7 +79,8 @@ $(document).ready(function(){
           type: 'doughnut',
           data: chartdata,
           options: {
-            responsive: false,
+            responsive: true,
+	           maintainAspectRatio: false,	
             circumference: Math.PI,
             rotation: 1.0 * Math.PI,
             percentageInnerCutout: 10,
