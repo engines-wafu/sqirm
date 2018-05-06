61f1e50d386620009b320616cd4cd0a146ae34ef
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 16:58:15 -0700

    controls.php development

--- www/html/controls.php
+++ www/html/controls.php
@@ -51,7 +51,7 @@ $hazard = $_GET["conID"]
             $result = mysqli_query($connection, $query); 
           
             while ($row = mysqli_fetch_array($result)) {
-            		echo '<p class="tile_hazard"><b><a href="RiskView.php?hazID=' . $row['hazID'] . '">' . $row['hazID'] . ' - ' . $row['hazDesc'] . '</a></b></p>' ;
+            		echo $row['conDesc'];
             }
           ?>
           </h2>
