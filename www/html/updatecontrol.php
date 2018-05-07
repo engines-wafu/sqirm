1a2469f99af082c009d47755acc0b1a9f3043695
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 21:42:26 -0700

    controls.php form

--- www/html/updatecontrol.php
+++ www/html/updatecontrol.php
@@ -8,11 +8,11 @@ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 $database = mysqli_select_db($connection, DB_DATABASE);
-$hazard = $_GET["conID"];
+$control = $_GET["conID"];
 
 $desc = $_POST['description'];
 
-$sql = "INSERT INTO controls (conDesc, conActive) VALUES ('" . "stuff" . "', 'Y')";
+$sql = "UPDATE controls SET conDesc = " . "'moar stuff'," . "conActive = 'Y' WHERE conID = " . $control;
 
 if (mysqli_query($connection, $sql)) {
   echo "New record created successfully";
