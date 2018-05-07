5d4ed0d4ee1938315e0b014636435041d1f0f986
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 17:58:29 -0700

    controls.php form

--- www/html/controls.php
+++ www/html/controls.php
@@ -62,12 +62,10 @@ $hazard = $_GET["conID"]
             		<label>Description</label><input type="text" name="descrpition" value="<?php echo $conDesc; ?>"><br />
             		<label>Active</label><input type="checkbox" name="active" <?php if ($conActive = "Y") echo 'checked';?>> <br />
             		<label>WRAG</label><input list="WRAG" name="WRAG" value="<?php echo $conWRAG ;?>"> <br />
-                <datalist name="WRAG">
-                  <option value="red">Volvo</option>
-                  <option value="amber">Saab</option>
-                  <option value="green">Fiat</option>
-                  <option value="white">Audi</option>
-                </datalist>
+              <input type="radio" name="WRAGradio" value="red" <?php if ($conWRAG = "red") echo 'checked';?>> Red<br>
+              <input type="radio" name="WRAGradio" value="amber" <?php if ($conWRAG = "amber") echo 'checked';?>> Amber<br>
+              <input type="radio" name="WRAGradio" value="green" <?php if ($conWRAG = "green") echo 'checked';?>> Green<br>
+              <input type="radio" name="WRAGradio" value="white" <?php if ($conWRAG = "white") echo 'checked';?>> White
             		<button type="submit" class="btn" name="reg_user">Update</button>
        	  		</form>
         </div>
