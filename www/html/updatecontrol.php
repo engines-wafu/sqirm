71413cd0e5a4569544cc7d3b3b3a15910ea041aa
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 21:50:48 -0700

    controls.php form

--- www/html/updatecontrol.php
+++ www/html/updatecontrol.php
@@ -10,10 +10,8 @@ if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect
 $database = mysqli_select_db($connection, DB_DATABASE);
 $control = $_GET["conID"];
 
-if(isset($_POST['submit']){
-  $desc = $_POST['description'];
-  $active = $_POST['description'];
-};
+$desc = $_POST['description'];
+echo 'This is the thing: ' . $_POST['description'];
 
 $sql = "UPDATE controls SET conDesc = " . $desc . "conActive = 'Y' WHERE conID = " . $control;
 
