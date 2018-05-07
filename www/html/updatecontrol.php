b95b43923097312ee15db0da78271990df522427
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 20:00:27 -0700

    controls.php form

--- www/html/updatecontrol.php
+++ www/html/updatecontrol.php
@@ -1,10 +1,23 @@
-          <?php
-            $sql = "INSERT INTO controls (conDesc, conActive)
-            VALUES ('Stuff in the thing', 'Y')";
-            
-            if (mysqli_query($connection, $sql)) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-            }
-          ?>
+<?php include "session.php"; ?>
+<?php include "../inc/dbinfo.inc"; ?>
+<?php
+
+/* Connect to MySQL and select the database. */
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$database = mysqli_select_db($connection, DB_DATABASE);
+$hazard = $_GET["conID"]
+
+?>
+<?php
+  $sql = "INSERT INTO controls (conDesc, conActive)
+  VALUES ('Stuff in the thing', 'Y')";
+
+if (mysqli_query($connection, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+}
+?>
