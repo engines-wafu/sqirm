dce723faf8486b6e6d5894b781e3c086e8bdea6d
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-06 18:03:57 -0700

    controls.php form

--- www/html/controls.php
+++ www/html/controls.php
@@ -61,10 +61,10 @@ $hazard = $_GET["conID"]
        	  		<form method="post" action="controls.php">
             		<label>Description</label><input type="text" name="descrpition" value="<?php echo $conDesc; ?>"><br />
             		<label>Active</label><input type="checkbox" name="active" <?php if ($conActive = "Y") echo 'checked';?>> <br />
-              <input type="radio" name="WRAGradio" value="red" <?php if ($conWRAG = "red") echo 'checked' endif;?>> Red<br>
-              <input type="radio" name="WRAGradio" value="amber" <?php if ($conWRAG = "amber") echo 'checked';?>> Amber<br>
-              <input type="radio" name="WRAGradio" value="green" <?php if ($conWRAG = "green") echo 'checked';?>> Green<br>
-              <input type="radio" name="WRAGradio" value="white" <?php if ($conWRAG = "white") echo 'checked';?>> White<br>
+              <input type="radio" name="WRAGradio" value="red" <?php if ($conWRAG == "red") echo 'checked';?>> Red<br>
+              <input type="radio" name="WRAGradio" value="amber" <?php if ($conWRAG == "amber") echo 'checked';?>> Amber<br>
+              <input type="radio" name="WRAGradio" value="green" <?php if ($conWRAG == "green") echo 'checked';?>> Green<br>
+              <input type="radio" name="WRAGradio" value="white" <?php if ($conWRAG == "white") echo 'checked';?>> White<br>
             		<button type="submit" class="btn" name="reg_user">Update</button>
             <?php echo $conWRAG; ?>
        	  		</form>
