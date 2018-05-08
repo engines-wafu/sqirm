5ac57f529e24030722f9f725a1a2e6c246f13ded
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-07 18:41:52 -0700

    Trying to tune size and layout of kpi charts

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -84,7 +84,7 @@ $hazard = $_GET["hazID"]
           $result = mysqli_query($connection, $query); 
           while ($row = mysqli_fetch_array($result)) {
             echo '<div id="kpicanvas">';
-            echo '   <canvas id="#kpicanvas' . $row['kpiID'] . '" class="kpi"></canvas>';
+            echo '   <canvas id="#kpicanvas #kpicanvas' . $row['kpiID'] . '"></canvas>';
             echo $row['kpiDesc'];
             echo '<div id="kpicanvas">';
           }
