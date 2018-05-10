6d1128012372041c15e2fb1d14eacde2ee88b59f
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-09 21:13:40 -0700

    Added logo

--- www/html/login.php
+++ www/html/login.php
@@ -15,7 +15,7 @@
   <article style="border: none;"> </article>
   <article>
     <div class="container">
-      <img src="img/logosm.png" width="150px" />
+      <img src="img/logosm.png" width="150px" align="center" />
       <form method="post" action="login.php">
         <label>Username</label> <input type="text" name="username"> <br />
         <label>Password</label> <input type="password" name="password"> <br />
