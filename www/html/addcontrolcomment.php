88d6d0c1b180381d8b1dbef8f6aa5bcb74453e0a
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-13 18:15:45 -0700

    Redoing the comment add on controls.php

--- www/html/addcontrolcomment.php
+++ www/html/addcontrolcomment.php
@@ -10,12 +10,19 @@ if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect
 $database = mysqli_select_db($connection, DB_DATABASE);
 
 $control = $_GET["conID"];
-$desc = $_POST['description'];
 $name = $_SESSION['username'];
 $date = date();
 $comment = $_POST['comment'];
 
-$sql = "INSERT INTO comments (comment, username, active, date) VALUES ('" . $comment . "', " . $name . "', 1, '" . $date "')";
+$sql = "INSERT INTO comments (comment, username, active, date) VALUES ('" . $comment . "', '" . $name . "', '1', '" . $date "')";
 
+if (mysqli_query($connection, $sql)) {
+  echo var_dump($_POST);
+  echo "Comment added";
+  header('Location: controls.php?conID=' . $control);
+  exit;
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+}
 
 ?>
