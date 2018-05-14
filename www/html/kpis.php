13c4260092f847bf5cf5d203b2a896a42e8ac507
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-14 12:09:22 -0700

    Adding action tile to welcome.php

--- www/html/kpis.php
+++ www/html/kpis.php
@@ -35,7 +35,7 @@ $hazard = $_GET["conID"]
           $query = "SELECT kpiID, kpiDesc FROM kpis";
           $result = mysqli_query($connection, $query); 
           while ($row = mysqli_fetch_array($result)) {
-            echo '<div>';
+            echo '<div class="cl">';
               echo '<div id="kpiblock">';
                 echo '<canvas id="#kpicanvas' . $row['kpiID'] . '" class="kpi"></canvas>';
                 echo '<div id="kpititle">' . $row['kpiDesc'] . '</div>';
