b0565827c9f4f0db25cd11332a3cbc7f5f62271d
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-13 18:07:56 -0700

    Redoing the comment add on controls.php

--- www/html/controls.php
+++ www/html/controls.php
@@ -138,7 +138,7 @@ $hazard = $_GET["conID"]
           <div class="cl">
 					<form action="addcontrolcomment.php?conID=<?php echo $conID ?>" name="commentControlAdd" method="post">
               <textarea id="comment" class="text" cols="70" rows ="10" name="comment">Insert new comment here.</textarea>
-              <input type="button" value="Submit">
+				      <input type="submit" value="Sumbit"/>
             </form>
           </div>
           <?php
