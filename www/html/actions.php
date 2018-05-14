84409cf47601e532310c9d7e6731df27ce8ce03e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-13 20:51:08 -0700

    Starting work on the actions.php page

--- www/html/actions.php
+++ www/html/actions.php
@@ -82,6 +82,72 @@ $hazard = $_GET["conID"]
         function myFunction() {
           // Declare variables
           var input, filter, table, tr, td, i;
+          input = document.getElementById("myInputDesc");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("myTable");
+          tr = table.getElementsByTagName("tr");
+        
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
+        </script>
+      <script>
+        function myFunction() {
+          // Declare variables
+          var input, filter, table, tr, td, i;
+          input = document.getElementById("myInputOwner");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("myTable");
+          tr = table.getElementsByTagName("tr");
+        
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
+        </script>
+      <script>
+        function myFunction() {
+          // Declare variables
+          var input, filter, table, tr, td, i;
+          input = document.getElementById("myInputComment");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("myTable");
+          tr = table.getElementsByTagName("tr");
+        
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
+        </script>
+      <script>
+        function myFunction() {
+          // Declare variables
+          var input, filter, table, tr, td, i;
           input = document.getElementById("myInputID");
           filter = input.value.toUpperCase();
           table = document.getElementById("myTable");
