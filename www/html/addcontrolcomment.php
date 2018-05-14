437fa4c5e7c141e4b9277264279c376d5c0e664f
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-13 18:56:18 -0700

    Redoing the comment add on controls.php

--- www/html/addcontrolcomment.php
+++ www/html/addcontrolcomment.php
@@ -16,20 +16,13 @@ $comment = $_POST['comment'];
 
 $sql = 'INSERT INTO comments (comment, username, date, active) VALUES ("' . $comment . '", "' . $name . '", "' . $date . '", 1)';
 
-if (mysqli_query($connection, $sql)) {
-  exit;
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-}
+mysqli_query($connection, $sql);
 
 $comID = mysqli_insert_id($connection);
 
 $sql = 'INSERT INTO comment_links (comID, conID) VALUES ("' . $comID . '", "' . $conID . '")';
 
-if (mysqli_query($connection, $sql)) {
-  exit;
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-}
+mysqli_query($connection, $sql)
+exit;
 
 ?>
