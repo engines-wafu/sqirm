2d0ddb17a2bf9c75d2d60cf811f09d586b2f04f5
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-15 21:42:18 -0700

    Trying to fix login.php redirect

--- www/html/kpis.php
+++ www/html/kpis.php
@@ -29,7 +29,7 @@ $hazard = $_GET["conID"]
     </div>
     <article>
       <?php include "navbar.php"; ?>
-      <div>
+      <div class="content">
         <?php
         $query = "SELECT kpiID, kpiDesc FROM kpis";
         $result = mysqli_query($connection, $query); 
