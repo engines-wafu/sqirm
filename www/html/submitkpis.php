6953c8676b3f234bc382238f3c61bfb693c7c7e3
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-16 18:49:13 -0700

    Working on KPI inputs

--- www/html/submitkpis.php
+++ www/html/submitkpis.php
@@ -9,17 +9,6 @@ if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect
 
 $database = mysqli_select_db($connection, DB_DATABASE);
 
-$name = $_SESSION['username'];
-$date = date('Y-m-d');
-$description = $_POST['kpiDesc'];
-$kpiID = $_POST['kpiID'];
-$val1 = $_POST['kpiVal1'];
-$desc1 = $_POST['kpiDesc1'];
-$val2 = $_POST['kpiVal2'];
-$desc2 = $_POST['kpiDesc2'];
-$val3 = $_POST['kpiVal3'];
-$desc3 = $_POST['kpiDesc3'];
-
 $sql = 'UPDATE kpis SET kpiDesc = "' . $description . '", kpiDesc1 = "' . $kpiDesc1 . '", kpiVal1 = "' . $kpiVal1 . '", kpiDesc2 = "' . $kpiDesc2 . '", kpiVal2 = "' . $kpiVal2 . '", kpiDesc3 = "' . $kpiDesc3 . '", kpiVal3 = "' . $kpiVal3 . '" WHERE kpiID = "' . $kpiID . '"';
 
 mysqli_query($connection, $sql);
