74b45abefcf75d38b5347839995f7345cde8cb8a
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-16 21:24:05 -0700

    Working on KPI inputs

--- www/html/deletekpi.php
+++ www/html/deletekpi.php
@@ -15,7 +15,7 @@ $sql = 'DELETE FROM kpis WHERE kpiID="' . $kpiID . '"';
 mysqli_query($connection, $sql);
 
 if (mysqli_query($connection, $sql)) {
-  echo var_dump($_POST);
+  header ('Location kpis.php');
   exit;
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($connection);
