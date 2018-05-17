0bcb63815eed6decf34a8e8d7df0fc38bf767149
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-16 20:15:58 -0700

    Working on KPI inputs

--- www/html/submitkpis.php
+++ www/html/submitkpis.php
@@ -9,13 +9,13 @@ if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect
 
 $database = mysqli_select_db($connection, DB_DATABASE);
 $kpiID = $_GET["kpiID"];
-$description = $_GET["kpiDesc"];
-$kpiDesc1 = $_GET["kpiDesc1"];
-$kpiVal1 = $_GET["kpiVal1"];
-$kpiDesc2 = $_GET["kpiDesc2"];
-$kpiVal2 = $_GET["kpiVal2"];
-$kpiDesc3 = $_GET["kpiDesc3"];
-$kpiVal3 = $_GET["kpiVal3"];
+$description = $_POST["kpiDesc"];
+$kpiDesc1 = $_POST["kpiDesc1"];
+$kpiVal1 = $_POST["kpiVal1"];
+$kpiDesc2 = $_POST["kpiDesc2"];
+$kpiVal2 = $_POST["kpiVal2"];
+$kpiDesc3 = $_POST["kpiDesc3"];
+$kpiVal3 = $_POST["kpiVal3"];
 
 $sql = 'UPDATE kpis SET kpiDesc = "' . $description . '", kpiDesc1 = "' . $kpiDesc1 . '", kpiVal1 = "' . $kpiVal1 . '", kpiDesc2 = "' . $kpiDesc2 . '", kpiVal2 = "' . $kpiVal2 . '", kpiDesc3 = "' . $kpiDesc3 . '", kpiVal3 = "' . $kpiVal3 . '" WHERE kpiID = "' . $kpiID . '"';
 
