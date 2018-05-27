853a701239e78e8ea8afa4f75bdac1a1979d512b
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 14:14:23 -0700

    Starting effectiveness scoring

--- www/html/controls.php
+++ www/html/controls.php
@@ -75,7 +75,9 @@ $result = mysqli_query($connection, $query);
 while ($row = mysqli_fetch_array($result)) {
   $kpiID = $row['kpiID'];
   $kpiDesc = $row['kpiDesc'];
-  echo 'test';
+  echo '<input type="radio" name="' . $kpiID . '"> ' . $kpiID . ' - ' . $kpiDesc ;
+  echo '</input>';
+  echo '<br>';
 }
 ?>
             <input type="submit" value="Sumbit"/>
