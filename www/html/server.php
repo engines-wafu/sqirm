b725fca27ff6025a71d22423b4d6b40d9c8b5c1e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-26 17:40:10 -0700

    Working on KPI inputs

--- www/html/server.php
+++ www/html/server.php
@@ -52,10 +52,8 @@ if (isset($_POST['login_user'])) {
 				$password = md5($password);
 				$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 				$results = mysqli_query($connection, $query);
-				if (mysqli_num_rows($results) == 1) {
-						$_SESSION['username'] = $username;
-						$_SESSION['success'] = "You are now logged in";
-						header('Location: kpis.php');
+				if(!isset($_SESSION['login_user'])){
+								header("location:login.php");
 				}else {
 						array_push($errors, "Wrong username/password combination");
 				}
