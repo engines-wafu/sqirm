73fe10b8ef13ced72a91528bef66d91ec30d5608
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 16:59:28 -0700

    Starting effectiveness scoring

--- www/html/score_effectiveness.php
+++ www/html/score_effectiveness.php
@@ -23,8 +23,30 @@ $control = $_GET["conID"]
 $query = "SELECT kpiDesc, kpiVal1, kpiVal2, kpiVal3, kpiVal3/(kpiVal1+kpiVal2+kpiVal3) AS kpiWeight FROM kpis INNER JOIN controls ON kpis.kpiID = controls.kpiPriID";
 $result = mysqli_query($connection, $query); 
 while ($row = mysqli_fetch_array($result)) {
-  echo '<p> The weighted kpi value of ' . $row['kpiDesc'] . ' is: ' .  $row['kpiWeight'] . '</p>';
+  $k = $row['kpiWeight'];
+  echo '<p> The weighted kpi value of ' . $row['kpiDesc'] . ' is: ' .  $k . '</p>';
 }
+
+$query = "SELECT conWRAG, conID FROM controls WHERE conID = " . $control;
+$result = mysqli_query($connection, $query); 
+while ($row = mysqli_fetch_array($result)) {
+  if ($row['conWRAG'] == 'white') {
+    $s = '0';
+  } elseif ($row['conWRAG'] == 'green') {
+    $s = '1';
+  }
+  } elseif ($row['conWRAG'] == 'yellow') {
+    $s = '0.5';
+  }
+  } elseif ($row['conWRAG'] == 'red') {
+    $s = '0.1';
+  }
+  echo '<p>Subjective score is: ' . $s . '</p>';
+}
+
+$e = $s * $k 
+
+echo '<p>Total effectiveness score is: ' . $e . '</p>';
 
 ?>
 
