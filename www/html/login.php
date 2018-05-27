4ae0b886c44e90a824416805652b38ab61620f17
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-26 22:00:28 -0700

    Completely new login method:

--- www/html/login.php
+++ www/html/login.php
@@ -42,44 +42,46 @@ require('layout/header.php');
   <article style="border:none;"> </article>
   <article style="border:none;">
     <div class="container">
-      <img src="img/logosm.png" width="150px" align="center" />
-   			<form role="form" method="post" action="" autocomplete="off">
-    				<h2>Please Login</h2>
-    				<p><a href='./'>Back to home page</a></p>
-    				<hr>
+      <div align=center style="border:none; padding-bottom:20px">
+        <img src="img/logosm.png" width="150px" align="center" />
+   		  	<form role="form" method="post" action="" autocomplete="off">
+    	  			<h2>Please Login</h2>
+    	  			<p><a href='./'>Back to home page</a></p>
+    	  			<hr>
     
-    				<?php
-    				//check for any errors
-    				if(isset($error)){
-    					foreach($error as $error){
-    						echo '<p class="bg-danger">'.$error.'</p>';
-    					}
-    				}
+    	  			<?php
+    	  			//check for any errors
+    	  			if(isset($error)){
+    	  				foreach($error as $error){
+    	  					echo '<p class="bg-danger">'.$error.'</p>';
+    	  				}
+    	  			}
     
-    				if(isset($_GET['action'])){
+    	  			if(isset($_GET['action'])){
     
-    					//check the action
-    					switch ($_GET['action']) {
-    						case 'active':
-    							echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
-    							break;
-    						case 'reset':
-    							echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
-    							break;
-    						case 'resetAccount':
-    							echo "<h2 class='bg-success'>Password changed, you may now login.</h2>";
-    							break;
-    					}
+    	  				//check the action
+    	  				switch ($_GET['action']) {
+    	  					case 'active':
+    	  						echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
+    	  						break;
+    	  					case 'reset':
+    	  						echo "<h2 class='bg-success'>Please check your inbox for a reset link.</h2>";
+    	  						break;
+    	  					case 'resetAccount':
+    	  						echo "<h2 class='bg-success'>Password changed, you may now login.</h2>";
+    	  						break;
+    	  				}
     
-    				}
-    				
-    				?>
+    	  			}
+    	  			
+    	  			?>
     
-    					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
-    					<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
-    						 <a href='reset.php'>Forgot your Password?</a>
-    				<hr>
-   			</form>
+    	  				<input type="text" name="username" id="username" input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
+    	  				<input type="password" name="password" id="password" input-lg" placeholder="Password" tabindex="3">
+    	  					 <a href='reset.php'>Forgot your Password?</a>
+    	  			<hr>
+   		  	</form>
+      </div>
     </div>
   </article>
   <article style="border:none;"> </article>
