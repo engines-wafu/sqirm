bfd556e48c62530daa69f4d1e8f3e57bb4cbe380
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-26 17:50:14 -0700

    Trying to fix login redirection

--- www/html/server.php
+++ www/html/server.php
@@ -55,6 +55,7 @@ if (isset($_POST['login_user'])) {
 				if (mysqli_num_rows($results) == 1) {
 						$_SESSION['username'] = $username;
 						$_SESSION['success'] = "You are now logged in";
+						array_push($errors, "Logged in");
 						header("location: welcome.php");
 				}else {
 						array_push($errors, "Wrong username/password combination");
