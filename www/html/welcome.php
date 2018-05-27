61c94fdc02840e2feb655cb8ea9aab00dbf4c165
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 12:26:11 -0700

    Aligning kpi blocks

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -81,7 +81,7 @@ $user = $_SESSION['username'];
           $result = mysqli_query($connection, $query); 
           while ($row = mysqli_fetch_array($result)) {
             echo '<div id="kpiblock">';
-			           echo '<table width="100%" style="border: none; table-layout:fixed" align=center><tr width="100%"><td>';	
+			           echo '<table width="40%" style="border: none; table-layout:fixed" align=center><tr width="40%"><td>';	
                 echo '<canvas vertical-align=top id="#kpicanvas' . $row['kpiID'] . '" class="kpi"></canvas>';
 			           echo '</td></tr><tr><td>';	
                 echo '<h3 align=center>' . $row['kpiDesc'] . '</h3>';
