67068983268910879e765f14f7d679842e74f24c
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-26 19:31:10 -0700

    Trying to fix login redirection

--- www/html/server.php
+++ www/html/server.php
@@ -57,7 +57,7 @@ if (isset($_POST['login_user'])) {
     if (mysqli_num_rows($results) == 1) {
       $_SESSION['username'] = $username;
       $_SESSION['success'] = "You are now logged in";
-      header("Location:https://www.blackcataerospace.com/welcome.php");
+      header("Location:/welcome.php");
     }else {
       array_push($errors, "Wrong username/password combination");
     }
