22e058abcb8cd4acaba6a8cc5b4222b22815f5d1
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-26 22:43:44 -0700

    Completely new login method:

--- www/html/index.php
+++ www/html/index.php
@@ -110,30 +110,52 @@ $title = 'sqirm';
 require('layout/header.php');
 ?>
 
-			<form role="form" method="post" action="" autocomplete="off">
-				<h2>Please Sign Up</h2>
-				<p>Already a member? <a href='login.php'>Login</a></p>
-				<hr>
+<section style="border:none; background:white;">	
+  <article style="border:none;"> </article>
+  <article style="border:none;">
+    <div class="container">
+      <div align=center style="border:none; padding:20px">
+        <img src="img/logosm.png" width="150px" align="center" />
+		     	<form role="form" method="post" action="" autocomplete="off">
+		     	 	<h2>Please Sign Up</h2>
+		     	 	<p>Already a member? <a href='login.php'>Login</a></p>
+		     	 	<hr>
 
-				<?php
-				//check for any errors
-				if(isset($error)){
-					foreach($error as $error){
-						echo '<p class="bg-danger">'.$error.'</p>';
-					}
-				}
+		     	 	<?php
+		     	 	//check for any errors
+		     	 	if(isset($error)){
+		     	 		foreach($error as $error){
+		     	 			echo '<p class="bg-danger">'.$error.'</p>';
+		     	 		}
+		     	 	}
 
-				//if action is joined show sucess
-				if(isset($_GET['action']) && $_GET['action'] == 'joined'){
-					echo "<h2 class='bg-success'>Registration successful, please check your email to activate your account.</h2>";
-				}
-				?>
+		     	 	//if action is joined show sucess
+		     	 	if(isset($_GET['action']) && $_GET['action'] == 'joined'){
+		     	 		echo "<h2 class='bg-success'>Registration successful, please check your email to activate your account.</h2>";
+		     	 	}
+		     	 	?>
 
-					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
-					<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['email'], ENT_QUOTES); } ?>" tabindex="2">
-							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
-							<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
-			</form>
+		     	 	<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
+		     	 	<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['email'], ENT_QUOTES); } ?>" tabindex="2">
+     	 			<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+     	 			<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+		     	</form>
+      </div>
+    </div>
+  </article>
+  <article style="border: none;">
+    <div class="alert">
+      <div>
+        <h3>
+        Disclaimer
+        </h3>
+        <p>
+          <?php include('disclaimer.txt'); ?>
+        </p>
+      </div>
+    </div>
+  </article>
+</section>
 
 <?php
 //include header template
