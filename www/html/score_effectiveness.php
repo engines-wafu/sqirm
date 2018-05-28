fa6b1fc06c86d7bd438fac6e86cf8d96d029393e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 17:15:47 -0700

    Starting effectiveness scoring

--- www/html/score_effectiveness.php
+++ www/html/score_effectiveness.php
@@ -44,7 +44,7 @@ while ($row = mysqli_fetch_array($result)) {
 
 $query = "SELECT actID FROM actions";
 $result = mysqli_query($connection, $query); 
-while ($row = mysqli_fetch_array($result)) {
+if ($row = mysqli_fetch_array($result)) {
   $n_t = mysqli_num_rows($result);
   echo '<p>Total number of actions is: ' . $n_t . '</p>';
 }
