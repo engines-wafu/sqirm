29b293d531ed9678d3ec96a7c412eabbe5342f16
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 23:17:24 -0700

    Starting effectiveness scoring

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -13,7 +13,79 @@ $user = $_SESSION['username'];
 $query = "SELECT conID FROM controls;";
 $result = mysqli_query($connection, $query); 
 while ($control = mysqli_fetch_array($result)) {
-  include 'control_score.php';
+  $query = "SELECT kpiPriID FROM controls WHERE conID = " . $control;
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $kpiPriID = $row['kpiPriID'];
+  }
+  
+  $query = "SELECT kpiDesc, kpiVal1, kpiVal2, kpiVal3, (kpiVal1 + kpiVal2)/(kpiVal1 + kpiVal2 + kpiVal3) AS kpiWeight FROM kpis WHERE kpiID = " . $kpiPriID;
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $k = $row['kpiWeight'];
+  } else {
+    $k = '1';
+  }
+  
+  $query = "SELECT conwrag, conid FROM controls WHERE conid = " . $control;
+  $result = mysqli_query($connection, $query); 
+  while ($row = mysqli_fetch_array($result)) {
+    if ($row['conwrag'] == 'white') {
+      $s = '0';
+    } elseif ($row['conwrag'] == 'green') {
+      $s = '1';
+    } elseif ($row['conwrag'] == 'yellow') {
+      $s = '0.5';
+    } elseif ($row['conwrag'] == 'red') {
+      $s = '0.1';
+    }
+  }
+  
+  $query = "SELECT conPriID FROM actions WHERE conPriID =" . $control;
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $n_i = mysqli_num_rows($result);
+  }
+  
+  $query = "SELECT actID FROM actions";
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $n_t = mysqli_num_rows($result);
+  }
+  
+  $e = $s * $k * (($n_t - $n_i) / $n_t) ;
+  
+  $query = "SELECT conID FROM consequence_control WHERE conID = " . $control;
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $n_cc = mysqli_num_rows($result);
+  }
+  
+  $query = "SELECT conID FROM threat_control WHERE conID = " . $control;
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $n_tc = mysqli_num_rows($result);
+  }
+  
+  $query = "SELECT conID FROM threat_control";
+  $result = mysqli_query($connection, $query); 
+  if ($row = mysqli_fetch_array($result)) {
+    $n_ta = mysqli_num_rows($result);
+  }
+  
+  $c = ($n_cc + $n_tc) / $n_ta;
+  
+  $t = hypot( $c, (1 - $e) );
+  
+  $query = 'UPDATE controls SET conScore = "' . $t . '" WHERE conID = "' . $control . '"';
+  if ($result = mysqli_query($connection, $query)) {; 
+  }
+  
+  ?>
+  <?php
+  
+    header('Location: controls.php?conID=' . $control);
+  ?>
 }
 
 ?>
