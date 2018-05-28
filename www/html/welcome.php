aa3b65e528c6fc60c0e39e9a874ba20a9921c93e
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 23:20:10 -0700

    Starting effectiveness scoring

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -12,7 +12,8 @@ $user = $_SESSION['username'];
 
 $query = "SELECT conID FROM controls;";
 $result = mysqli_query($connection, $query); 
-while ($control = mysqli_fetch_array($result)) {
+while ($row = mysqli_fetch_array($result)) {
+  $control = $row['conID'];
   $query = "SELECT kpiPriID FROM controls WHERE conID = " . $control;
   $result = mysqli_query($connection, $query); 
   if ($row = mysqli_fetch_array($result)) {
