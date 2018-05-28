bf523f3db03f7c4726ee6ec4fa5a67a8bc3d0e62
Gavin Edwards <edwards.gavin@gmail.com>
2018-05-27 23:09:16 -0700

    Starting effectiveness scoring

--- www/html/welcome.php
+++ www/html/welcome.php
@@ -13,7 +13,6 @@ $user = $_SESSION['username'];
 $query = "SELECT conID FROM controls;";
 $result = mysqli_query($connection, $query); 
 while ($row = mysqli_fetch_array($result)) {
-  header('Location: control_score.php?conID=' . $row['conID']);
 }
 
 ?>
