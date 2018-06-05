395e8930129a574a0288433fb0a13c75d61b427d
Gavin Edwards <edwards.gavin@gmail.com>
2018-06-05 14:05:18 -0700

    Adding new action feature.

--- www/html/updateaction.php
+++ www/html/updateaction.php
@@ -24,7 +24,7 @@ if ($_POST["WRAGradio"] == "red"){
   $wrag = "white";
 }
 
-$sql = 'UPDATE actions SET actIssue = "' . $description . '", actOwner = "' . $owner . '", actWRAG = "' . $wrag . '", actDue "' . $dl. '", actTopic = "' . $topic . '", actOrigin = "' . $origin . '" WHERE actID = "' . $action . '"';
+$sql = 'UPDATE actions SET actIssue = "' . $description . '", actOwner = "' . $owner . '", actWRAG = "' . $wrag . '", actDue ' . $dl. ', actTopic = "' . $topic . '", actOrigin = "' . $origin . '" WHERE actID = "' . $action . '"';
 
 if (mysqli_query($connection, $sql)) {
   header('Location: action.php?actID=' . $action );
