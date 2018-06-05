14c0c6a929387ce5aa486fe7b9e79d487221bcfa
Gavin Edwards <edwards.gavin@gmail.com>
2018-06-05 13:47:56 -0700

    Adding new action feature.

--- www/html/updateaction.php
+++ www/html/updateaction.php
@@ -11,6 +11,9 @@ $database = mysqli_select_db($connection, DBNAME);
 $action = $_GET["actID"];
 $description = $_POST["description"];
 $owner = $_POST["owner"];
+$topic = $_POST["topic"];
+$origin = $_POST["origin"];
+$dl = $_POST["dl"];
 if ($_POST["WRAGradio"] == "red"){
   $wrag = "red";
 } elseif ($_POST["WRAGradio"] == "amber") {
@@ -21,13 +24,10 @@ if ($_POST["WRAGradio"] == "red"){
   $wrag = "white";
 }
 
-$sql = 'UPDATE actions SET actIssue = "' . $description . '", actOwner = "' . $owner . '", actWRAG = "' . $wrag . '" WHERE actID = "' . $action . '"';
+$sql = 'UPDATE actions SET actIssue = "' . $description . '", actOwner = "' . $owner . '", actWRAG = "' . $wrag . '", actOrigin = '" . $origin . "', actTopic = '" . $topic . "', actDL = '" . $dl . "' WHERE actID = "' . $action . '"';
 
 if (mysqli_query($connection, $sql)) {
-  echo $description;
-  echo $owner;
-  echo $wrag;
-  #header('Location: action.php?actID=' . $action );
+  header('Location: action.php?actID=' . $action );
   exit;
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($connection);
