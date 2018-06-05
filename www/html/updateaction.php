a534474a10b8be1fb14d13fddb6713488b87f3d5
Gavin Edwards <edwards.gavin@gmail.com>
2018-06-05 13:27:56 -0700

    Adding new action feature.

--- www/html/updateaction.php
+++ www/html/updateaction.php
@@ -26,7 +26,8 @@ if ($_POST["red"] == "checked") {
 $sql = 'UPDATE actions SET actIssue = "' . $description . '", actOwner = "' . $owner . '", actWRAG = "' . $wrag . '" WHERE actID = "' . $action . '"';
 
 if (mysqli_query($connection, $sql)) {
-  print_r($_POST);
+  $arr = get_defined_vars();
+  echo $arr;
   #header('Location: action.php?actID=' . $action );
   exit;
 } else {
