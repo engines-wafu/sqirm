7be13e4915daa7a20e42769ac846972c93010476
Gavin Edwards <edwards.gavin@gmail.com>
2018-06-14 18:04:08 -0700

    Formatting on actions.php

--- www/html/RiskView.php
+++ www/html/RiskView.php
@@ -102,7 +102,9 @@ while ($row3 = mysqli_fetch_array($result1)) {
 ?>
      <tr>
      <td>
-<?php <form method="POST" action="addnewthreat.php?hazard=" . $hazard >; ?>
+<?php 
+echo '<form method="POST" action="addnewthreat.php?hazard="' . $hazard . '>'; 
+?>
           <textarea name="description" rows="5" cols="60">Enter new threat here.</textarea>
           <br>
      </td>
